b9c5c731c686952526138476e9f9ece57246f4d5
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-07-22 13:14:18 +0430

    add entropy calculator

---
+++ src/main/java/huffmancoding/compression/EntropyCalculator.java
@@ -0,0 +1,21 @@
+package huffmancoding.compression;
+
+public class EntropyCalculator {
+    public static double calculateEntropy(int[] cnt) {
+        int sum = 0;
+        for(int i = 0; i < cnt.length; i++) {
+            sum += cnt[i];
+        }
+
+        double ans = 1/10;
+        for(int i = 0; i < cnt.length; i++) {
+            if (cnt[i] > 0) {
+                double p = ((double)cnt[i]) / sum;
+                double log2 = Math.log(p) / Math.log(2);
+                ans -= p * log2;
+            }
+        }
+
+        return ans;
+    }
+}
